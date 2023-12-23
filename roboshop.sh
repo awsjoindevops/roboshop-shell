1284dcde185628d76f0785bd9b8a4ef4966130ce
awsdevopsonline <punnam.elite@gmail.com>
2023-12-23 22:42:17 +0530

    greeting commit

--- roboshop.sh
+++ roboshop.sh
@@ -7,6 +7,9 @@ SG_ID=sg-0af7c8cc8d4bcb618
 #--security-group-ids sg-0af7c8cc8d4bcb618
 
 
+ZONE_ID=Z0052568278EYAYP4D07R
+DOMAIN_NAME=awsjoindevops.online
+
 INSTANCES=("cart" "catalogue" "mongodb" "mysql" "redis" "rabbitmq" "shipping"
  "payment" "user" "web" "dispatch")
 
@@ -23,4 +26,25 @@ fi
 IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
 echo "$i : $IP_ADDRESS"
+
+
+#CREATE ROUTE 53
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$i'.'$DOMAIN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP_ADDRESS'"
+        }]
+      }
+    }]
+  }'
+
 done
