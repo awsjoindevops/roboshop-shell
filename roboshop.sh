150c2c677b3ce0245ecc8d949d75f237fecfc966
awsdevopsonline <punnam.elite@gmail.com>
2023-12-26 20:07:31 +0530

    ansible mongodb commit

--- roboshop.sh
+++ roboshop.sh
@@ -35,7 +35,7 @@ aws route53 change-resource-record-sets \
   {
     "Comment": "Testing creating a record set"
     ,"Changes": [{
-      "Action"              : "CREATE"
+      "Action"              : "UPSERT"
       ,"ResourceRecordSet"  : {
         "Name"              : "'$i'.'$DOMAIN_NAME'"
         ,"Type"             : "A"
